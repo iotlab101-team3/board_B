acddfb64a8986a609765546ae3808dd67db7f4da
yeonbeen <{lyb0228@naver.com}>
2021-12-03 11:04:31 +0900

    부저 핀 변경 및 display 변경

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <SSD1306.h>
 
-#define speakerpin 5 
+#define speakerpin 14
 #define bpmControl_MSB 12
 #define bpmControl_LSB 13
 #define buttonSW 2
@@ -11,6 +11,7 @@ SSD1306    display(0x3c, 4, 5, GEOMETRY_128_32);
 int first = 1319; //메트로놈 첫박 소리 높이
 int other = 1047; //메트로놈 다른 소리 높이
 float bpm;
+float bpm_delay;
 // float bpm1;
 // float bpm2; //가변저항에 따른 박자 속도 계산
 int count = 1;
@@ -46,7 +47,6 @@ IRAM_ATTR void buttonClicked() {
   }
 }
 
-
 void setup() {
     Serial.begin(115200);
     display.init();
@@ -67,24 +67,21 @@ void setup() {
 void loop() {
   if((encoderValue >= 60) && (encoderValue <= 180))
   {
-    bpm = (60 / (float)encoderValue) * 850;
+    bpm = encoderValue;
+    bpm_delay = (60 / (float)encoderValue) * 850;
     
-    /* bpm1 = 60 / (float)encoderValue;
-    bpm2 = bpm1 * 850; //가변저항 값에 따라 메트로놈 속도 변환
-    bpm2 = bpm1 * 1000; */
-
     if (count%rhythm == 1) { //첫 박마다 소리 출력
       tone(speakerpin, first, 150);
       delay(150);
       noTone(speakerpin);
-      delay(bpm);
+      delay(bpm_delay);
       count++; //count 값 증가
     }
     else { //첫박을 제외한 다른 박 소리 출력
         tone(speakerpin, other, 150);
         delay(150);
         noTone(speakerpin);
-        delay(bpm);
+        delay(bpm_delay);
         count++; //count 값 증가
     }
 
